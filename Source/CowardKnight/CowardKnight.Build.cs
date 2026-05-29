abd72dc13a0be507f08fa92b20adc1637addafff
minjoon-and <minjoon.and@gmail.com>
2026-05-29 22:01:21 +0900

    build: GameplayTags 모듈 의존성 추가

--- Source/CowardKnight/CowardKnight.Build.cs
+++ Source/CowardKnight/CowardKnight.Build.cs
@@ -8,7 +8,7 @@ public class CowardKnight : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags" });
 		PublicIncludePaths.Add(ModuleDirectory);
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
